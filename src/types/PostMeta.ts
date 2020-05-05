ce3d9cd574e4e6e1a8e513e5e876e0e14a6be99b
hota1024 <hotalog@hota1024.com>
2020-05-06 01:40:14 +0900

    🏷️ Add src/types/PostMeta.ts

---
+++ src/types/PostMeta.ts
@@ -0,0 +1,19 @@
+/*
+ * PostMeta data type.
+ */
+export type PostMeta = {
+  /**
+   * Post title.
+   */
+  title: string
+
+  /**
+   * Post tags array.
+   */
+  tags: string[]
+
+  /**
+   * Post thumbnail url.
+   */
+  thumbnail?: string
+}
