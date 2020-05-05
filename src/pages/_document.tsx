72f9477cc6a7c5312581887bae8c44271d58771a
hota1024 <hotalog@hota1024.com>
2020-05-05 20:22:55 +0900

    Add src/pages/_document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>hotalog</title>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
