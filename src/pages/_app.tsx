abb82b5ad35413c32a6d205ef21d35c8bd314cab
hota1024 <hotalog@hota1024.com>
2020-05-06 02:45:43 +0900

    Update src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,16 @@
 import App from 'next/app'
-import { MainLayout } from '../layouts/Main'
 import Head from 'next/head'
-import { PostLayout } from '../layouts/Post'
 
 export default class extends App {
   render() {
     const { Component, pageProps } = this.props
-    let isPost = false
-
-    if (Component['isMDXComponent'] && !Component['noPost']) {
-      isPost = true
-    }
 
     return (
       <>
         <Head>
           <title>hotalog</title>
         </Head>
-        {isPost ? (
-          <PostLayout>
-            <Component {...pageProps} />
-          </PostLayout>
-        ) : (
-          <MainLayout>
-            <Component {...pageProps} />
-          </MainLayout>
-        )}
+        <Component {...pageProps} />
       </>
     )
   }
