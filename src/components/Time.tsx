d12fd397111172ccc2a7554e22bdca8df3718458
hota1024 <hotalog@hota1024.com>
2020-05-06 19:01:43 +0900

    Add src/components/Time.tsx

---
+++ src/components/Time.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react'
+import dayjs from 'dayjs'
+
+export type TimeProps = {
+  time: string
+  color?: string
+}
+
+export const Time: FC<TimeProps> = (props) => {
+  const time = dayjs(props.time)
+  const color = props.color ?? 'inherit'
+
+  return (
+    <>
+      <time className="time" dateTime={time.toString()}>
+        {time.format('YYYY/MM/DD')}
+      </time>
+
+      <style jsx>{`
+        .time {
+          color: ${color};
+        }
+      `}</style>
+    </>
+  )
+}
