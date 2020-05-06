25c7d76f1788e2c84c0ac93b2c9e8952d1de22b3
hota1024 <hotalog@hota1024.com>
2020-05-06 15:36:03 +0900

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
+import { Card } from '../components/Card'
+import { CardContent } from '../components/CardContent'
+import { Image } from '../components/Image'
+import { MainLayout } from '../layouts/Main'
+
 export default function Home() {
-  return <></>
+  return (
+    <>
+      <MainLayout>
+        <Card>
+          <Image src="/code.jpg" />
+          <CardContent>Hello world</CardContent>
+        </Card>
+      </MainLayout>
+    </>
+  )
 }
