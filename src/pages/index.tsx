fe1c49ab6a6a5d6cfb89b52b090a81e502988113
hota1024 <hotalog@hota1024.com>
2020-05-08 16:04:43 +0900

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,59 @@
-import { Card } from '../components/Card'
-import { CardContent } from '../components/CardContent'
-import { Image } from '../components/Image'
 import { MainLayout } from '../layouts/Main'
+import { FC } from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { PostData } from '../types/PostData'
+import { PostCard } from '../components/PostCard'
 
-export default function Home() {
+const Home: FC<{ posts: PostData[] }> = (props) => {
   return (
     <>
       <MainLayout>
-        <Card>
-          <Image src="/thumbnail-0.png" />
-          <CardContent>Hello world</CardContent>
-        </Card>
+        <div className="posts">
+          {props.posts.map((post, key) => (
+            <PostCard key={key} post={post} />
+          ))}
+        </div>
       </MainLayout>
+
+      <style jsx>{`
+        .posts {
+          margin: 64px auto;
+          max-width: 1200px;
+          display: grid;
+          justify-content: center;
+          grid-template-columns: repeat(auto-fit, minmax(378px, 1fr));
+          column-gap: 32px;
+          row-gap: 32px;
+        }
+      `}</style>
     </>
   )
+}
+
+export default Home
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: ['/'],
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const contexts = require.context('./', true, /\.mdx$/)
+  const posts: PostData[] = []
+
+  contexts.keys().forEach((key) => {
+    const meta = contexts(key).meta ?? {}
+    posts.push({
+      name: key.replace(/\.mdx$/, ''),
+      ...meta,
+    })
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
