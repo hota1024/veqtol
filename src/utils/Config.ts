f4f1819ead9e1e138beb0b746048614dce13b4d5
hota1024 <hotalog@hota1024.com>
2020-05-09 01:16:50 +0900

    Update src/utils/Config.ts

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -2,3 +2,16 @@ import veqtolJson from '../../veqtol.json'
 import { VeqtolConfig } from '../types/VeqtolConfig'
 
 export const config = veqtolJson as VeqtolConfig
+
+/**
+ * Get configuration value or default value.
+ *
+ * @param key Keyof config.
+ * @param defaultValue Default value.
+ */
+export function get<K extends keyof VeqtolConfig>(
+  key: K,
+  defaultValue?: VeqtolConfig[K]
+) {
+  return config[key] ?? defaultValue
+}
