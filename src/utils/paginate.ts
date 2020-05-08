e1a12a91a8e7f5f8ef8aa5eedea05e3c80236507
hota1024 <hotalog@hota1024.com>
2020-05-08 16:20:27 +0900

    Update src/utils/paginate.ts

--- src/utils/paginate.ts
+++ src/utils/paginate.ts
@@ -5,7 +5,7 @@
  * @param pageSize Items per page.
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function Paginate<T extends any[]>(array: T, pageSize: number): T[][] {
+export function Paginate<T extends any[]>(array: T, pageSize: number): T[] {
   return array.reduce(
     (newArray, _, i) =>
       i % pageSize ? newArray : [...newArray, array.slice(i, i + pageSize)],
