0e4c8634b653e6cc237ce307a491a24fc4f6dee0
hota1024 <hotalog@hota1024.com>
2020-05-08 16:12:07 +0900

    Update src/components/PostList.tsx

---
+++ src/components/PostList.tsx
@@ -0,0 +1,31 @@
+import { PostData } from '../types/PostData'
+import { FC } from 'react'
+import { PostCard } from './PostCard'
+
+export type PostListProps = {
+  posts: PostData[]
+}
+
+export const PostList: FC<PostListProps> = (props) => {
+  return (
+    <>
+      <div className="post-list">
+        {props.posts.map((post, key) => (
+          <PostCard key={key} post={post} />
+        ))}
+      </div>
+
+      <style jsx>{`
+        .post-list {
+          margin: 64px auto;
+          max-width: 1200px;
+          display: grid;
+          justify-content: center;
+          grid-template-columns: repeat(auto-fit, minmax(378px, 1fr));
+          column-gap: 32px;
+          row-gap: 32px;
+        }
+      `}</style>
+    </>
+  )
+}
