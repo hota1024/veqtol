88cb0cde5d602fbd9437d89f1633fb0f82a730df
hota1024 <hotalog@hota1024.com>
2020-05-09 17:05:40 +0900

    Add src/components/Tags.tsx

---
+++ src/components/Tags.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react'
+import clsx from 'clsx'
+import { Chip } from './Chip'
+import { Get } from '../utils/Config'
+
+export type TagsProps = {
+  tags: string[]
+  className?: string
+}
+
+export const Tags: FC<TagsProps> = (props) => {
+  return (
+    <>
+      <div className={clsx(props.className)}>
+        {props.tags.map((tag, key) => (
+          <Chip
+            key={key}
+            href={tag}
+            color={Get('tagColor')}
+            textColor={Get('tagText')}
+          >
+            {tag}
+          </Chip>
+        ))}
+      </div>
+    </>
+  )
+}
