8d9dade8939b6b655ffe96e84de94ea0bfe2f788
hota1024 <hotalog@hota1024.com>
2020-05-10 01:01:25 +0900

    Update src/utils/GetPostsContexts.tsx

--- src/utils/GetPostsContexts.tsx
+++ src/utils/GetPostsContexts.tsx
@@ -1,4 +1,5 @@
 /**
  * Returns posts require.contexts.
  */
-export const GetPostsContexts = () => require.context('../', true, /\.mdx$/)
+export const GetPostsContexts = () =>
+  require.context('../pages', true, /\.mdx$/)
