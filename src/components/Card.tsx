3ee0147e39ea146e20a3e7566bb82a8b9b209a82
hota1024 <hotalog@hota1024.com>
2020-05-09 16:50:26 +0900

    Update src/components/Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -18,9 +18,9 @@ export const Card: FC<CardProps> = (props) => {
       <style jsx>{`
         .card {
           background: ${color};
+          color: inherit;
           border-radius: 4px;
           ${!noShadow && 'box-shadow: 0 0 8px rgba(0, 0, 0, 0.14);'}
-          background: white;
           display: block;
           max-width: 100%;
           overflow: hidden;
