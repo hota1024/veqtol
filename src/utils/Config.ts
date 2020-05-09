03d486b0e6ea028d56420c70483a2e44d9e69257
hota1024 <hotalog@hota1024.com>
2020-05-09 16:49:29 +0900

    Update src/utils/Config.ts

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,6 +1,25 @@
 import veqtolJson from '../../veqtol.json'
 import { VeqtolConfig } from '../types/VeqtolConfig'
 
+export const defaultConfig: Partial<VeqtolConfig> = {
+  titleColor: 'inherit',
+  descriptionColor: 'inherit',
+  background: '#f6f6f6',
+  color: '#202020',
+  postColor: 'white',
+  postText: 'inherit',
+  postHeader1BorderColor: '#202020',
+  inlineCodeColor: 'rgba(200, 200, 200, 0.4)',
+  inlineCodeText: 'inherit',
+  tagColor: '#f0f0f0',
+  tagText: '#202020',
+  timeColor: '#a0a0a0',
+  buttonColor: 'white',
+  buttonText: '#202020',
+  postPaginationPrevText: 'Prev',
+  postPaginationNextText: 'Next',
+}
+
 export const config = veqtolJson as VeqtolConfig
 
 /**
@@ -9,9 +28,9 @@ export const config = veqtolJson as VeqtolConfig
  * @param key Keyof config.
  * @param defaultValue Default value.
  */
-export function get<K extends keyof VeqtolConfig>(
+export function Get<K extends keyof VeqtolConfig>(
   key: K,
   defaultValue?: VeqtolConfig[K]
 ) {
-  return config[key] ?? defaultValue
+  return config[key] ?? defaultValue ?? defaultConfig[key]
 }
