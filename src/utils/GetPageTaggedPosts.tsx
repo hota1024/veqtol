5245df2a2041509c883974ce7c72fb1968635348
hota1024 <hotalog@hota1024.com>
2020-05-10 23:24:27 +0900

    Add src/utils/GetPageTaggedPosts.tsx

---
+++ src/utils/GetPageTaggedPosts.tsx
@@ -0,0 +1,26 @@
+import dayjs from 'dayjs'
+import { GetPage } from './GetPage'
+import { GetPostsData } from './GetPostsData'
+
+/**
+ * Returns page tagged posts.
+ *
+ * @param tag Tag.
+ * @param page Number of page.
+ * @param pageSize Items per page.
+ */
+export const GetPageTaggedPosts = (
+  tag: string,
+  page: number,
+  pageSize = 12
+) => {
+  return GetPage(
+    GetPostsData()
+      .filter(({ tags }) => !!tags.find((t) => t === tag))
+      .sort((a, b) =>
+        dayjs(a.updatedAt).isBefore(dayjs(b.updatedAt)) ? 1 : -1
+      ),
+    page,
+    pageSize
+  )
+}
