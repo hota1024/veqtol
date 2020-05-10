0a26ff94909050101f4c19a5cf58338d799674b8
hota1024 <hotalog@hota1024.com>
2020-05-10 20:09:31 +0900

    Add src/types/TagData.ts

---
+++ src/types/TagData.ts
@@ -0,0 +1,16 @@
+import { PostData } from './PostData'
+
+/**
+ * TagData type.
+ */
+export type TagData = {
+  /**
+   * Tag name.
+   */
+  name: string
+
+  /**
+   * Posts array.
+   */
+  posts: PostData[]
+}
