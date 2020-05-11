cccc53f44142954acab7ae7cb16a1bacdf6512b1
hota1024 <hotalog@hota1024.com>
2020-05-12 00:07:23 +0900

    Add src/utils/PrismThemes.ts

---
+++ src/utils/PrismThemes.ts
@@ -0,0 +1,27 @@
+import dracula from 'prism-react-renderer/themes/dracula'
+import duotoneDark from 'prism-react-renderer/themes/duotoneDark'
+import duotoneLight from 'prism-react-renderer/themes/duotoneLight'
+import github from 'prism-react-renderer/themes/github'
+import nightOwl from 'prism-react-renderer/themes/nightOwl'
+import nightOwlLight from 'prism-react-renderer/themes/nightOwlLight'
+import oceanicNext from 'prism-react-renderer/themes/oceanicNext'
+import palenight from 'prism-react-renderer/themes/palenight'
+import shadesOfPurple from 'prism-react-renderer/themes/shadesOfPurple'
+import synthwave84 from 'prism-react-renderer/themes/synthwave84'
+import ultramin from 'prism-react-renderer/themes/ultramin'
+import vsDark from 'prism-react-renderer/themes/vsDark'
+
+export const PrismThemes = {
+  dracula,
+  duotoneDark,
+  duotoneLight,
+  github,
+  nightOwl,
+  nightOwlLight,
+  oceanicNext,
+  palenight,
+  shadesOfPurple,
+  synthwave84,
+  ultramin,
+  vsDark,
+}
