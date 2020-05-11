0aead8142e134d5446ebbc0051020570d0d6b6ec
hota1024 <hotalog@hota1024.com>
2020-05-11 17:56:13 +0900

    Update src/components/Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,43 @@
+import { useContext } from 'react'
+import { CommonContext } from '@/pages/_app'
+import { Tags } from './Tags'
+import { Get } from '@/utils'
+
 export const Footer = () => {
+  const { tagNames } = useContext(CommonContext)
+
   return (
     <>
-      <footer></footer>
+      <footer className="footer">
+        <div className="footer-tags">
+          <h4 className="footer-tags-title">Tags</h4>
+          <Tags tags={tagNames} />
+        </div>
+        {Get('copyright') && (
+          <div className="footer-copyright">{Get('copyright')}</div>
+        )}
+      </footer>
+
+      <style jsx>{`
+        .footer-tags {
+          border-top: 1px solid rgba(0, 0, 0, 0.2);
+          padding: 64px 0;
+          text-align: center;
+        }
+
+        .footer-tags-title {
+          font-size: 2rem;
+          margin: 0;
+          margin-bottom: 32px;
+        }
+
+        .footer-copyright {
+          background: ${Get('copyrightColor')};
+          color: ${Get('copyrightText')};
+          text-align: center;
+          padding: 24px 0;
+        }
+      `}</style>
     </>
   )
 }
