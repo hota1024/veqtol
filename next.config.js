6eb40634a1c4d4699ad1556da26646c1c2a26527
hota1024 <hotalog@hota1024.com>
2020-05-11 13:35:10 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
@@ -6,4 +7,8 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   exportTrailingSlash: true,
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  },
 })
